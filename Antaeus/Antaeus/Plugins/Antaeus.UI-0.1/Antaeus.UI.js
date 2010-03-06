4c05d823bf28815e8b15974836eae6e015357b27
lanslot <lanslot.liu@gmail.com>
2010-03-06 12:39:28 +0800

    把Tab部分的代码全部抽取出来了
    Signed-off-by: lanslot <lanslot.liu@gmail.com>

--- Antaeus/Antaeus/Plugins/Antaeus.UI-0.1/Antaeus.UI.js
+++ Antaeus/Antaeus/Plugins/Antaeus.UI-0.1/Antaeus.UI.js
@@ -12,7 +12,6 @@ function FormLoginSubmit(place) {
 	sUsername = $("#FormLogin" + place + " #Username").attr("value");
 	sPassword = $("#FormLogin" + place + " #Password").attr("value");
 	sRememberMe = $("#FormLogin" + place + " #RememberMe").attr("value");
-	//alert(sUsername+","+sPassword+","+sRememberMe);
 	if (sUsername == "" || sUsername == "昵称或邮箱地址" || sPassword == "") {
 		alert("用户名和密码都不能为空！");
 	} else {
@@ -25,7 +24,7 @@ function FormLoginSubmit(place) {
 					if (data.indexOf("error:") >= 0) {
 						$(sPopup+" #LoginErrorMessage").html(data.replace("error:", ""));
 						$(sPopup+" input#Username").attr("value", sUsername);
-						DialogLoad(sPopup.replace("#",""));
+						DialogLoad(sPopup.replace("#",""),function(){FormLoginSubmit("Popup");});
 						$(sPopup).dialog("open");
 						return false;
 					} else {
@@ -33,7 +32,7 @@ function FormLoginSubmit(place) {
 					}
 				//如果是Popup登陆
 				} else {
-					if (data.indexOf("LoginErrorMessage") >= 0) {
+					if (data.indexOf("error:") >= 0) {
 						alert("登录失败：" + data.replace("error:", ""));
 					} else {
 						$("#logon").html(data);
@@ -47,11 +46,9 @@ function FormLoginSubmit(place) {
 	}
 }
 
-
-// filter方法用于动态提供筛选
-// Parameters:
-// [必须]ajax - 字符 - 发送请求的URL
-
+//filter方法用于动态提供筛选
+//Parameters:
+//[必须]ajax - 字符 - 发送请求的URL
 (function($){  
 	$.fn.extend({   
 	filter: function(options){
@@ -115,3 +112,62 @@ function FormLoginSubmit(place) {
 	}
 	});      
 })(jQuery); 
+
+//DialogLoad函数用于执行基本的Dialog调用设置
+function DialogLoad(ID,fun){
+    $("#"+ID).dialog({ //这是对话框的基本设置
+        autoOpen: false,
+        width: 600,
+        buttons: {
+            "Ok": function() {
+				fun();
+            },
+            "Cancel": function() {
+                $(this).dialog("close");
+            }
+        },
+		draggable: true,
+		modal: true,
+		resizable:true,
+		show:"slide"
+    });
+}
+
+//DialogRun函数用于执行Dialog的全部行为
+// 关联于DialogLoad函数		
+//============================================================================
+//如果一个链接点击时需要激发Dialog，请在这个链接的HTML代码标签内加入rel='dialog'
+//下面是这个操作的代码，激发后将动态产生一个DIV层，来载入Dialog
+//关于Dialog的设置属性，请参见前面的DialogLoad函数的设定，这个函数将再次判断ID来确定“OK”按钮和“Cancel”按钮的操作
+function dialogRun(ID,fun) {  //这是点击相关链接触发对话框事件的定义
+	var target = $("#"+ID).attr("href");
+	
+	//创建一个加载Popup的DIV，由于同页面可能有多个Popup，因此通过随机数产生ID
+	var divID="Dialog"+String(parseInt(Math.random()*100000));		
+	$("body").append("<div id='"+divID+"' class='hidden'></div>");
+	var divObj = $("#"+divID);
+	
+	//判断是直接性DIV载入还是AJAX载入
+	if(target[1]!="#"){ //直接DIV载入
+		divObj.html($(target).html());
+		divObj.attr("title",$(target).attr("title"));
+		var Dialog = DialogLoad(divID,fun);
+		divObj.dialog("open");
+	}else{ //AJAX载入
+		//为了避免网速问题，加入载入中的显示
+		divObj.attr("title","内容载入中...");
+		divObj.html("内容载入中...");
+		divObj.load(target.substring(2), {}, function() {
+			//还是因为滞后性因此两个都要写，一个直接改控件中的标题，一个是希望在控件在载入前改标题
+			divObj.attr("title",divObj.children().attr("title"));
+			$(".ui-dialog-title").html(divObj.children().attr("title"));				
+		});
+		//由于AJAX载入的滞后性，下面两行代码不能与前面判断相同的内容一起提取到if外面执行
+		var Dialog = DialogLoad(divID,fun);
+		divObj.dialog("open");
+	}
+	return false;
+}
+			
+
+
