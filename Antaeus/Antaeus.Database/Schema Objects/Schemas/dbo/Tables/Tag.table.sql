a9bf958df09fd543b649c615908b30d8969cb8cb
ningliaoyuan <ninglioayuan@gmail.com>
2010-03-30 21:40:54 +0800

    Add missing db file: Tag.table.sql
    Signed-off-by: ningliaoyuan <ninglioayuan@gmail.com>

---
+++ Antaeus/Antaeus.Database/Schema Objects/Schemas/dbo/Tables/Tag.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Tag]
+(
+	[TagID] BIGINT NOT NULL,
+    [KID] VARCHAR (600)	NOT NULL,    
+	[CrUserName]  NVARCHAR (400) NOT NULL,
+    [CreatedTime]	DATETIME NOT NULL,
+    [Tags] VARCHAR (600)	NOT NULL
+)
